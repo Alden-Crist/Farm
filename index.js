98eafe4c471888327e4517c637a61e0284017bf3
Alden-Crist <aldencristrego08@gmail.com>
2024-07-27 16:52:13 +0530

    done

--- index.js
+++ index.js
@@ -1,16 +1,122 @@
+// const fs = require('fs');
+// const http = require('http');
+// const url = require('url');
+// const slugify = require('slugify');
+// const replaceTemplate = require('./modules/replaceTemplate');
+// /////////////////////////////////////////////
+// //Files
+
+// // Blocking ,synchronous way
+// // const textIn = fs.readFileSync("./starter/txt/input.txt", "utf-8");
+// // console.log(textIn);
+
+// // const textOut = `This is what we know about the avacado: ${textIn}.\nCreated on ${Date.now()}`;
+// // fs.writeFileSync("./starter/txt/output.txt", textOut);
+// // console.log("File written");
+
+// // Non-Blocking, asynchronous way
+// // fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+// //   if (err) return console.log("Error");
+// //   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+// //     console.log(data2);
+// //     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+// //       console.log(data3);
+// //       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+// //         console.log("Your file has been ready");
+// //       });
+// //     });
+// //   });
+// // });
+
+// // console.log("Will read file");
+
+// /////////////////////////////////////////////
+// //SERVER
+// const tempOverview = fs.readFileSync(
+//   `${__dirname}/templates/template-overview.html`,
+//   'utf-8'
+// );
+// //SERVER
+
+// const tempCard = fs.readFileSync(
+//   `${__dirname}/templates/template-card.html`,
+//   'utf-8'
+// );
+// //SERVER
+// const tempProduct = fs.readFileSync(
+//   `${__dirname}/templates/template-product.html`,
+//   'utf-8'
+// );
+// const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+// const dataObject = JSON.parse(data);
+
+// const slugs = dataObject.map((el) => slugify(el.productName, { lower: true }));
+
+// console.log(slugs);
+
+// const server = http.createServer((req, res) => {
+//   // console.log(req);
+//   // console.log(req.url);
+//   // res.end("Hello from the server!");
+//   // console.log(req.url);
+//   // console.log(url.parse(req.url, true));
+//   const myURL = new URL(`http://127.0.0.1:8000${req.url}`);
+//   const { searchParams, pathname: pathName } = myURL;
+
+//   //Overview page
+//   if (pathName === '/' || pathName === '/overview') {
+//     res.writeHead(200, {
+//       'content-type': 'text/html',
+//     });
+//     const cardsHtml = dataObject
+//       .map((el) => replaceTemplate(tempCard, el))
+//       .join('');
+//     const output = tempOverview.replace('{%PRODUCT_CARDS%', cardsHtml);
+
+//     res.end(output);
+
+//     //Product page
+//   } else if (pathName === '/product') {
+//     console.log(searchParams);
+//     res.writeHead(200, { 'Content-type': 'text/html' });
+//     const product = dataObject.find((el) => el.id == searchParams.get('id'));
+//     output = replaceTemplate(tempProduct, product);
+//     res.end(output);
+//     //api
+//   } else if (pathName === '/api') {
+//     res.writeHead(200, {
+//       'content-type': 'application/json',
+//     });
+//     res.end(data);
+//     //not found
+//   } else {
+//     res.writeHead(404, {
+//       'Content-type': 'text/html',
+//       'my-own-header': 'hello-world',
+//     });
+//     res.end('<h1>Page could not be found</h1>');
+//   }
+// });
+
+// server.listen(8000, '127.0.0.1', () => {
+//   console.log('Listening to requests on port 8000');
+// });
+
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
 const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
-/////////////////////////////////////////////
-//Files
+require('dotenv').config(); // Load environment variables
 
-// Blocking ,synchronous way
+/////////////////////////////////////////////
+// Files
+
+// Blocking, synchronous way
 // const textIn = fs.readFileSync("./starter/txt/input.txt", "utf-8");
 // console.log(textIn);
 
-// const textOut = `This is what we know about the avacado: ${textIn}.\nCreated on ${Date.now()}`;
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
 // fs.writeFileSync("./starter/txt/output.txt", textOut);
 // console.log("File written");
 
@@ -31,18 +137,15 @@ const replaceTemplate = require('./modules/replaceTemplate');
 // console.log("Will read file");
 
 /////////////////////////////////////////////
-//SERVER
+// SERVER
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   'utf-8'
 );
-//SERVER
-
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
   'utf-8'
 );
-//SERVER
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
   'utf-8'
@@ -55,15 +158,10 @@ const slugs = dataObject.map((el) => slugify(el.productName, { lower: true }));
 console.log(slugs);
 
 const server = http.createServer((req, res) => {
-  // console.log(req);
-  // console.log(req.url);
-  // res.end("Hello from the server!");
-  // console.log(req.url);
-  // console.log(url.parse(req.url, true));
-  const myURL = new URL(`http://127.0.0.1:8000${req.url}`);
+  const myURL = new URL(`${req.protocol}://${req.headers.host}${req.url}`);
   const { searchParams, pathname: pathName } = myURL;
 
-  //Overview page
+  // Overview page
   if (pathName === '/' || pathName === '/overview') {
     res.writeHead(200, {
       'content-type': 'text/html',
@@ -75,20 +173,22 @@ const server = http.createServer((req, res) => {
 
     res.end(output);
 
-    //Product page
+  // Product page
   } else if (pathName === '/product') {
     console.log(searchParams);
     res.writeHead(200, { 'Content-type': 'text/html' });
     const product = dataObject.find((el) => el.id == searchParams.get('id'));
-    output = replaceTemplate(tempProduct, product);
+    const output = replaceTemplate(tempProduct, product);
     res.end(output);
-    //api
+
+  // API
   } else if (pathName === '/api') {
     res.writeHead(200, {
       'content-type': 'application/json',
     });
     res.end(data);
-    //not found
+
+  // Not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
@@ -98,6 +198,9 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to requests on port 8000');
+const PORT = process.env.PORT || 8000;
+const HOST = process.env.HOST || '127.0.0.1';
+
+server.listen(PORT, HOST, () => {
+  console.log(`Listening to requests on ${HOST}:${PORT}`);
 });
